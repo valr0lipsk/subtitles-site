b9c6ecdb1dbdb203b71bad85ac93b83893804a66
Valeryia Lipskaya <valery.lipskaya03@gmail.com>
2023-02-24 10:09:07 +0300

    Add swiper.

---
+++ js/index.js
@@ -0,0 +1,9 @@
+const swiper = new Swiper(".swiper", {
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  slidesPerView: 2,
+  spaceBetween: 30,
+});
